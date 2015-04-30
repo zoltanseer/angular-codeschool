755183cb13fd73471dcf5ee6c9fbd83613a3c8b1
Zoltan Seer <zoltan.seer@bss-neon.de>
2015-04-30 12:31:14 +0200

    Some more directives + filters

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-var app = angular.module('store', []);
+(function() {
+	var app = angular.module('store', []);
+
+	app.controller('StoreController', function () {
+		this.products = gems;
+	});
+
+	var gems = [
+		{
+			name: 'Dodecahedron',
+			price: 2.95,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, ad, ut. Neque magnam inventore maiores suscipit itaque ab sequi, laudantium excepturi ex quos perferendis, nam officia reiciendis dolor nisi accusantium.',
+			canPurchase: true,
+			soldOut: false
+		},
+		{
+			name: 'Pentagonal gem',
+			price: 5,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, ad, ut. Neque magnam inventore maiores suscipit itaque ab sequi, laudantium excepturi ex quos perferendis, nam officia reiciendis dolor nisi accusantium.',
+			canPurchase: true,
+			soldOut: false
+		}
+	];
+})();
+
